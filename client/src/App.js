3115934761453574ca0930610c7321bb3845f2f5
Raine Wallace <rainebeaudubs@gmail.com>
2019-11-15 13:08:10 -0500

    added simple and quite ugly navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './components/nav/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <h1>Welcome to Richy's Painting Website!</h1>
       </header>
---
+++ client/src/components/nav/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navigation.css'
+
+const Navigation = () => {
+  return (
+    <div className="nav">
+      <Link className="navlink" to="/">Richy's Masterpieces</Link>
+      <div>
+        <Link className="navlink" to="/">About</Link> 
+        <Link className="navlink" to="/">Commissions</Link>
+        <Link className="navlink" to="/">Gallery</Link>
+        <Link className="navlink" to="/">Locations</Link>
+        <Link className="navlink" to="/">Store</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation; 
